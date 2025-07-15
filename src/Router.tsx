7ac47f70340f56f7c7a08d8035047bbfb2e47300
minsu0707 <mandoo4137@gmail.com>
2025-07-15 17:28:59 +0900

    fix: Router 설정

--- src/Router.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import { Home } from "./pages";
-
-export const Router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
