3257f556df62e3484453ea6afbf02ddddf85502a
은지 <wej0513@dsm.hs.kr>
2025-07-15 16:42:15 +0900

    feat: 문제 컴포넌트 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QuizCard } from "./components/QuizCard";
+import { GlobalStyle } from "./styles/GlobalStyles";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<QuizCard num={7} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
