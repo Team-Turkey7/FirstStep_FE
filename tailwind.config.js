d11a311ed60a1a16a7c00f8ffb4632b9062b68d6
minsu0707 <mandoo4137@gmail.com>
2025-07-15 12:34:39 +0900

    feat: color system

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          10: "#03FF7B",
+          20: "#00E7B6",
+          30: "#04CBE8",
+          40: "#05AAFF",
+          50: "#0084FE",
+          60: "#0057D6",
+          70: "#78C8F2",
+        },
+      },
+    },
   },
   plugins: [],
 };
