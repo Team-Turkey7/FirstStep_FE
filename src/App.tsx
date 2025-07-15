e27f86a75a8528bb844c01029b9e7e434accc718
은지 <wej0513@dsm.hs.kr>
2025-07-16 08:34:19 +0900

    feat: 메인 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Onboarding } from "./pages/Onboarding";
+import { Main } from "./pages/Main";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Onboarding />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Main.tsx
@@ -0,0 +1,73 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import DayButton from "../components/DayButton";
+import { colors } from "../styles";
+import MyIcon from "../assets/img/MyIcon.svg";
+import Study from "../assets/img/Study.svg";
+
+export const Main = () => {
+  return (
+    <div css={Container}>
+      <img css={My} src={MyIcon} />
+      <img css={StudyIcon} src={Study} />
+      <p css={Title}>학습하기</p>
+      <p css={Content}>
+        성장을 위한 가장 든든한<span css={Important}>첫걸음</span>, <br />
+        지금 시작하세요
+      </p>
+      <div css={ButtonGrid}>
+        {Array.from({ length: 30 }, (_, i) => (
+          <DayButton key={i + 1} day={(i + 1).toString()} isCompleted={true} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Container = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  gap: 20px;
+  padding: 60px 32px;
+`;
+
+const My = css`
+  width: 48px;
+  height: 48px;
+  display: flex;
+  align-self: flex-end;
+`;
+
+const StudyIcon = css`
+  width: 154px;
+  height: 120px;
+`;
+
+const Title = css`
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+`;
+
+const Content = css`
+  color: #6c6c6c;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+`;
+
+const Important = css`
+  color: ${colors.colors.main[40]};
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+`;
+
+const ButtonGrid = css`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 32px;
+  margin-top: 20px;
+`;
