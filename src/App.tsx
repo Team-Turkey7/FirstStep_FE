f53b4c16e760d4877b2b911dba733a6b627f6741
은지 <wej0513@dsm.hs.kr>
2025-07-16 10:06:33 +0900

    feat: 과목 선택 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Onboarding } from "./pages/Onboarding";
 import { Main } from "./pages/Main";
+import { Study } from "./pages/Study";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Onboarding />} />
         <Route path="/main" element={<Main />} />
+        <Route path="/study" element={<Study />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Study.tsx
@@ -0,0 +1,116 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import KoreanIcon from "../assets/img/Korean.svg";
+import EnglishIcon from "../assets/img/English.svg";
+import MathIcon from "../assets/img/Math.svg";
+import { colors } from "../styles";
+import Speaker from "../components/Speaker";
+import backIcon from "../assets/img/backIcon.svg";
+
+const Topic = [
+  { icon: KoreanIcon, name: "한글" },
+  { icon: EnglishIcon, name: "영어" },
+  { icon: MathIcon, name: "연산" },
+];
+
+export const Study = () => {
+  return (
+    <div css={Container}>
+      <div css={Header}>
+        <img css={BackButton} src={backIcon} />
+        <p css={DayText}>1일차</p>
+      </div>
+      <div css={Content}>
+        <p css={Title}>학습하기</p>
+        <Speaker />
+        <div css={ButtonWrapper}>
+          {Topic.map((index) => (
+            <div css={TopicButton}>
+              <img css={Icon} src={index.icon} />
+              <p css={Subject}>{index.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Container = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+  background: white;
+  padding-top: 58px;
+`;
+
+const Header = css`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
+
+const BackButton = css`
+  background: none;
+  border: none;
+  padding: 0;
+  position: absolute;
+  left: 20px;
+  width: 24px;
+  height: 24px;
+`;
+
+const DayText = css`
+  flex: 1;
+  text-align: center;
+  font-size: 32px;
+  font-weight: 700;
+`;
+
+const Content = css`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-top: 48px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = css`
+  font-size: 27px;
+  font-weight: 700;
+  margin-bottom: 28px;
+`;
+
+const ButtonWrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  margin-top: 100px;
+`;
+
+const TopicButton = css`
+  display: flex;
+  width: 123px;
+  height: 52px;
+  align-items: center;
+  gap: 12px;
+  padding: 22px 142px 22px 75px;
+  background-color: ${colors.colors.main[40]};
+  border-radius: 15px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
+
+const Icon = css``;
+
+const Subject = css`
+  color: white;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 600;
+`;
